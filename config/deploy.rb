b9208736c099d327dbff0d8f01827bb6b050fad0
Raul Palacios <palacios.raul02@gmail.com>
2017-01-03 17:19:02 -0600

    cambie

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, "https://github.com/RpL02/deploy_test.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
- set :deploy_to, "/home/deploy/deploy_test"
+ set :deploy_to, "/home/root/deploy_test"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
